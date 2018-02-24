2709c284072e7471b59b92bc6062f4ad3af3dcdf
Jake Callery <jcallery@subvoicestudios.com>
2018-02-24 14:30:49 -0500

    presets now standalone

--- client/js/Preset.js
+++ client/js/Preset.js
@@ -1,8 +1,7 @@
 import l from 'jac/logger/Logger';
 import EventUtils from 'jac/utils/EventUtils';
 import EventDispatcher from 'jac/events/EventDispatcher';
-import GlobalEventBus from 'jac/events/GlobalEventBus';
-import JacEvent from './jac/events/JacEvent';
+import MarkerDataObject from "./MarkerDataObject";
 
 const MODES = {
     LOOP: 'loop',
@@ -10,13 +9,71 @@ const MODES = {
 };
 
 export default class Preset extends EventDispatcher {
-    constructor($view){
+    constructor($presetButtonView, $modeButtonView){
         super();
+
+        let self = this;
+
 
         this.startSample = null;
         this.endSample = null;
         this.mode = Preset.MODES.LOOP;
+        this.isSet = false;
 
+        this.markerDO = new MarkerDataObject();
+
+        this.currentPSDownButton = null;
+        this.currentPSDownTime = null;
+        this.currentPSDownDelay = null;
+
+        //DOM
+        this.presetButtonView = $presetButtonView;
+        this.modeButtonView = $modeButtonView;
+        this.doc = this.presetButtonView.ownerDocument;
+
+        //Delegates
+        this.presetMouseDownDelegate = EventUtils.bind(self, self.handlePresetMouseDown);
+        this.presetClickDelegate = EventUtils.bind(self, self.handlePresetClick);
+        this.globalMosueUpDelegate = EventUtils.bind(self, self.handleGlobalMouseUp);
+
+        //Events
+        this.presetButtonView.addEventListener('click', this.presetClickDelegate);
+        this.presetButtonView.addEventListener('mousedown', this.presetMouseDownDelegate);
+
+        l.debug('New Preset: ', this.presetButtonView.id);
+
+    }
+
+    handleGlobalMouseUp($evt){
+        l.debug('Caught Global Mouse Up: ', $evt.target, $evt.currentTarget);
+        this.doc.removeEventListener('mouseup', this.globalMosueUpDelegate);
+    }
+
+    handlePresetMouseDown($evt){
+        l.debug('PS Mouse Down');
+        this.currentPSDownTime = Date.now();
+        this.doc.addEventListener('mouseup', this.globalMosueUpDelegate);
+    }
+
+    handlePresetClick($evt) {
+        this.currentPSDownDelay = Date.now() - this.currentPSDownTime;
+        l.debug('Delay: ', this.currentPSDownDelay);
+
+        if(this.currentPSDownDelay > 500){
+            l.debug('Set preset');
+            this.startSample = this.markerDO.startMarkerSample;
+            this.endSample = this.markerDO.endMarkerSample;
+            this.isSet = true;
+        } else {
+            l.debug('Use Preset');
+            if(this.isSet){
+                this.markerDO.updateStartSample(this.startSample);
+                this.markerDO.updateEndSample(this.endSample);
+            } else {
+                l.debug('Preset not set');
+            }
+
+        }
     }
 
     static get MODES() {
--- client/js/PresetManager.js
+++ client/js/PresetManager.js
@@ -1,24 +1,13 @@
 import l from 'jac/logger/Logger';
-import EventUtils from 'jac/utils/EventUtils';
 import EventDispatcher from 'jac/events/EventDispatcher';
-import GlobalEventBus from 'jac/events/GlobalEventBus';
-import JacEvent from './jac/events/JacEvent';
 import Preset from 'Preset';
-import MarkerDataObject from "./MarkerDataObject";
-import PresetDataObject from "./PresetDataObject";
 
 export default class PresetManager extends EventDispatcher {
     constructor($doc){
         super($doc);
         this.doc = $doc;
 
-        this.presetDO = new PresetDataObject();
-        this.markderDO = new MarkerDataObject();
-
-        this.currentPSDownButton = null;
-        this.currentPSDownTime = null;
-        this.currentPSDownDelay = null;
-
+        this.presets = [];
 
         //Wait for the DOM to be ready
         this.doc.addEventListener('DOMContentLoaded', () => {
@@ -37,59 +26,14 @@ export default class PresetManager extends EventDispatcher {
         this.ps3Button = this.doc.getElementById('preset3Button');
         this.ps4Button = this.doc.getElementById('preset4Button');
 
-        //Delegates
-        this.presetMouseDownDelegate = EventUtils.bind(self, self.handlePresetMouseDown);
-        this.presetClickDelegate = EventUtils.bind(self, self.handlePresetClick);
-        this.globalMosueUpDelegate = EventUtils.bind(self, self.handleGlobalMouseUp);
-
-        //Events
-        this.ps1Button.addEventListener('click', this.presetClickDelegate);
-        this.ps2Button.addEventListener('click', this.presetClickDelegate);
-        this.ps3Button.addEventListener('click', this.presetClickDelegate);
-        this.ps4Button.addEventListener('click', this.presetClickDelegate);
-
-        this.ps1Button.addEventListener('mousedown', this.presetMouseDownDelegate);
-        this.ps2Button.addEventListener('mousedown', this.presetMouseDownDelegate);
-        this.ps3Button.addEventListener('mousedown', this.presetMouseDownDelegate);
-        this.ps4Button.addEventListener('mousedown', this.presetMouseDownDelegate);
-
+        //Setup presets
+        this.presets.push(new Preset(this.ps1Button));
+        this.presets.push(new Preset(this.ps2Button));
+        this.presets.push(new Preset(this.ps3Button));
+        this.presets.push(new Preset(this.ps4Button));
 
     }
 
-    handleGlobalMouseUp($evt){
-        l.debug('Caught Global Mouse Up: ', $evt.target, $evt.currentTarget);
-        this.doc.removeEventListener('mouseup', this.globalMosueUpDelegate);
-    }
 
-    handlePresetMouseDown($evt){
-        l.debug('PS Mouse Down');
-        this.currentPSDownTime = Date.now();
-        this.doc.addEventListener('mouseup', this.globalMosueUpDelegate);
-    }
-
-    handlePresetClick($evt) {
-        this.currentPSDownDelay = Date.now() - this.currentPSDownTime;
-        l.debug('Delay: ', this.currentPSDownDelay);
-        let psButtonId = $evt.target.id;
-
-        if(this.currentPSDownDelay > 500){
-            l.debug('Set preset');
-            l.debug('Target: ', psButtonId);
-            this.presetDO.presetDict[psButtonId] = {
-                start:this.markderDO.startMarkerSample,
-                end:this.markderDO.endMarkerSample
-            }
-        } else {
-            l.debug('Use Preset');
-            l.debug('ID: ', psButtonId, psButtonId in this.presetDO.presetDict);
-            if(psButtonId in this.presetDO.presetDict){
-                l.debug('Has Prop');
-                this.markderDO.updateStartSample(this.presetDO.presetDict[psButtonId].start);
-                this.markderDO.updateEndSample(this.presetDO.presetDict[psButtonId].end);
-
-            }
-
-        }
-    }
 }
 
