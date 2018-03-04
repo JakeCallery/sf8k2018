898d3e5d5ac7d832af8dac134a1add91d4c41dcc
Jake Callery <jcallery@subvoicestudios.com>
2018-03-03 19:58:53 -0500

    mute button layout / svg load

--- client/js/LayoutManager.js
+++ client/js/LayoutManager.js
@@ -27,6 +27,8 @@ export default class LayoutManager extends EventDispatcher {
         this.leftControlsDiv = this.doc.getElementById('leftControlsDiv');
         this.rightControlsDiv = this.doc.getElementById('rightControlsDiv');
 
+        this.muteButton = this.doc.getElementById('muteButton');
+
         this.presetDiv = this.doc.getElementById('presetDiv');
         this.presetButtons = this.doc.getElementsByClassName('presetButton');
 
@@ -43,31 +45,38 @@ export default class LayoutManager extends EventDispatcher {
         let leftControlsWidth = Math.round(0.1 * viewportWidth);
         let rightControlsWidth = Math.round(0.2 * viewportWidth);
 
-        this.leftControlsDiv.style.width = leftControlsWidth + 'px';
-        this.rightControlsDiv.style.width = rightControlsWidth + 'px';
+        this.leftControlsDiv.style['width'] = leftControlsWidth + 'px';
+        this.rightControlsDiv.style['width'] = rightControlsWidth + 'px';
 
-        this.canvasContainerDiv.style.width = canvasWidth + 'px';
+        this.canvasContainerDiv.style['width'] = canvasWidth + 'px';
 
         this.waveCanvas.width = canvasWidth;
         this.soundCanvas.width = canvasWidth;
 
-        this.waveCanvas.style.width = canvasWidth + 'px';
-        this.soundCanvas.style.width = canvasWidth + 'px';
+        this.waveCanvas.style['width'] = canvasWidth + 'px';
+        this.soundCanvas.style['width'] = canvasWidth + 'px';
 
+        //Mute Button
+        let muteButtonWidth = Math.round(leftControlsWidth * 0.9);
+        let muteButtonHeight = Math.round(leftControlsWidth * 0.9);
+        let muteButtonMargin = Math.round((leftControlsWidth * 0.1) / 2);
+        this.muteButton.style['width'] = muteButtonWidth + 'px';
+        this.muteButton.style['height'] = muteButtonHeight + 'px';
+        this.muteButton.style['margin'] = muteButtonMargin + 'px';
 
-        //Button horizontal margins
+        //Right Controls Button horizontal margins
         let buttonLeftMargin = Math.round((rightControlsWidth * 0.02) / 1.5);
 
         //Preset Button Sizing
-        this.presetDiv.style.height = this.soundCanvas.height + 'px';
+        this.presetDiv.style['height'] = this.soundCanvas.height + 'px';
         let presetButtonWidth = Math.round(rightControlsWidth * 0.48);
         let presetButtonHeight = Math.round((this.soundCanvas.height * 0.90) / this.presetButtons.length);
         let presetButtonVerticalMargin = Math.round(((this.soundCanvas.height * 0.1) / this.presetButtons.length) / 2);
         presetButtonVerticalMargin += Math.round(presetButtonVerticalMargin / (this.presetButtons.length + 1));
 
         for(let presetButton of this.presetButtons) {
-            presetButton.style.width = presetButtonWidth + 'px';
-            presetButton.style.height = presetButtonHeight + 'px';
+            presetButton.style['width'] = presetButtonWidth + 'px';
+            presetButton.style['height'] = presetButtonHeight + 'px';
             presetButton.style['margin-top'] = presetButtonVerticalMargin + 'px';
             presetButton.style['margin-bottom'] = presetButtonVerticalMargin + 'px';
             presetButton.style['margin-left'] = buttonLeftMargin + 'px';
@@ -75,15 +84,15 @@ export default class LayoutManager extends EventDispatcher {
         }
 
         //Mode Button Sizing
-        this.modeDiv.style.height = this.soundCanvas.height + 'px';
+        this.modeDiv.style['height'] = this.soundCanvas.height + 'px';
         let modeButtonWidth = Math.round(rightControlsWidth * 0.48);
         let modeButtonHeight = Math.round((this.soundCanvas.height * 0.90) / this.modeButtons.length);
         let modeButtonVerticalMargin = Math.round(((this.soundCanvas.height * 0.1) / this.modeButtons.length) / 2);
         modeButtonVerticalMargin += Math.round(modeButtonVerticalMargin / (this.modeButtons.length + 1));
 
         for(let modeButton of this.modeButtons) {
-            modeButton.style.width = modeButtonWidth + 'px';
-            modeButton.style.height = modeButtonHeight + 'px';
+            modeButton.style['width'] = modeButtonWidth + 'px';
+            modeButton.style['height'] = modeButtonHeight + 'px';
             modeButton.style['margin-top'] = modeButtonVerticalMargin + 'px';
             modeButton.style['margin-bottom'] = modeButtonVerticalMargin + 'px';
             modeButton.style['margin-left'] = buttonLeftMargin + 'px';
