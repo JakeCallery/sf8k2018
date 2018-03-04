4e0c82ca7351209cd24f1d78276b5bea0ada4b70
Jake Callery <jcallery@subvoicestudios.com>
2018-03-04 16:14:48 -0500

    basic thumb drawing on touchPad implemented

--- client/js/VolPanTouchPadUI.js
+++ client/js/VolPanTouchPadUI.js
@@ -38,6 +38,19 @@ export default class VolPanTouchPadUI extends EventDispatcher {
         //Clear Canvas
         this.volPanTouchPadCanvasContext.fillStyle = '#FF00FF';
         this.volPanTouchPadCanvasContext.fillRect(0,0,this.volPanTouchPadCanvas.width, this.volPanTouchPadCanvas.height);
+
+        //Draw thumb
+        let padWidth = this.volPanTouchPadCanvas.width;
+        let padHeight = this.volPanTouchPadCanvas.height;
+
+        let padWidthCenter = padWidth/2;
+        let thumbX = (this.volPanDO.currentPan / 100) * (padWidthCenter) + padWidthCenter;
+        let thumbY = padHeight - ((this.volPanDO.currentVolume / 100) * (padHeight));
+
+        this.volPanTouchPadCanvasContext.beginPath();
+        this.volPanTouchPadCanvasContext.fillStyle = '#00FF00';
+        this.volPanTouchPadCanvasContext.arc(thumbX, thumbY, (padWidth * 0.1), 0, Math.PI * 2, false);
+        this.volPanTouchPadCanvasContext.fill();
         this.rafId = requestAnimationFrame(this.requestAnimationFrameDelegate);
     }
 }
